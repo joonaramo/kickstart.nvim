b22acf0798480fbb4a33cbfa96b91e060c700d92
joonaramo <joona00ramo@gmail.com>
2024-02-27 23:03:44 +0200

    add copy path to oil

--- lua/custom/plugins/oil.lua
+++ lua/custom/plugins/oil.lua
@@ -3,7 +3,11 @@ return {
   opts = {},
   -- Optional dependencies
   dependencies = { "nvim-tree/nvim-web-devicons" },
-  config = function ()
-    require("oil").setup()
+  config = function()
+    require("oil").setup({
+      keymaps = {
+        ["gy"] = "actions.copy_entry_path",
+      }
+    })
   end
 }
