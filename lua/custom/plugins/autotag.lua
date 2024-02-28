838ec6054ffbc5910d5a9bd2c09ed54775c3df9e
joonaramo <joona00ramo@gmail.com>
2024-02-28 09:06:37 +0200

    add autopairs back, remove autotag

--- lua/custom/plugins/autotag.lua
+++
@@ -1,7 +0,0 @@
-return {
-  "windwp/nvim-ts-autotag",
-  ft = { "html", "javascript", "javascriptreact", "typescriptreact", "svelte", "vue" },
-  config = function()
-    require("nvim-ts-autotag").setup()
-  end,
-}
