860c8486f16e8b50e4e75e4b392a81f4ca56c2ea
joonaramo <joona00ramo@gmail.com>
2024-06-12 23:03:14 +0300

    null-ls -> none-ls

--- lua/custom/plugins/null-ls.lua
+++ lua/custom/plugins/null-ls.lua
@@ -1,17 +1,18 @@
 return {
-  "jose-elias-alvarez/null-ls.nvim",
-  event = "LspAttach",
+  'nvimtools/none-ls.nvim',
+  dependencies = {
+    'nvimtools/none-ls-extras.nvim',
+  },
+  event = 'LspAttach',
   config = function()
-    local nls = require("null-ls")
-    nls.setup(
-      {
-        sources = {
-          nls.builtins.formatting.prettierd,
-          require("typescript.extensions.null-ls.code-actions"),
-          nls.builtins.code_actions.eslint_d,
-          nls.builtins.diagnostics.eslint_d,
-        }
-      }
-    )
+    local nls = require 'null-ls'
+    nls.setup {
+      sources = {
+        nls.builtins.formatting.prettierd,
+        nls.builtins.formatting.stylua,
+        require 'none-ls.diagnostics.eslint_d',
+        require 'none-ls.code_actions.eslint_d',
+      },
+    }
   end,
 }
